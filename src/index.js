7522145452a77d4e3fdb10957e0fd0e5b6513f51
TomasTMI <5055238+TomasTMI@users.noreply.github.com>
2023-10-09 12:09:14 +0200

    Hello a Hola

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 const version = "1.0.0";
 
 const hello = (name) => {
-  return("Hello " + name);
+  return("Hola " + name);
 }
-
-// console.log(version);
-// console.log(hello("Tomás"));
 
 export {version, hello}
